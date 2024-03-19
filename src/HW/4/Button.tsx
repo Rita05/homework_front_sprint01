a8c538faa3c795e118d11a92129bc3b05682e90a
Rita05 <redunm@mail.ru>
2024-03-19 10:40:37 +0300

    make HW4 homework

--- src/HW/4/Button.tsx
+++ src/HW/4/Button.tsx
@@ -1,16 +1,17 @@
 type ButtonPropsType = {
-  callBack: any // НУЖНО ПРОТИПИЗИРОВАТЬ
-  name: any // НУЖНО ПРОТИПИЗИРОВАТЬ
+  callBack: () => void
+  name: string
 };
 
 export const Button = (props: ButtonPropsType) => {
+  const { name, callBack } = props;
   const callBackHandler = () => {
-    // НУЖНО ДОПИСАТЬ
+    callBack();
   };
 
   return (
     <button id={'hw04-button'} onClick={callBackHandler}>
-      {props.name}
+      {name}
     </button>
   );
 };
